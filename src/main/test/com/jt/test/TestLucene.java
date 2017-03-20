a9049c3a01df8995c83a5871f764747979e7629e
王兆琦 <wzq15511597382@gmail.com>
2017-03-20 21:57:05 +0800

    Add files via upload

---
+++ src/main/test/com/jt/test/TestLucene.java
@@ -0,0 +1,24 @@
+package com.jt.test;
+
+import org.apache.lucene.document.Document;
+import org.junit.Test;
+
+/**
+ * 测试Lucene 和Solr
+ * Created by 王兆琦  on 2017/2/23 19.31.
+ */
+public class TestLucene {
+
+    @Test
+    public void testLucene() {
+
+        Document document = new Document();
+
+    }
+
+    @Test
+    public void testSolr() {
+
+    }
+
+}
